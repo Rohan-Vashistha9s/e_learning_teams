4846b166cf7bf726c18adf3b4ad44fa461f1ff25
anubhootipandey <anubhootipandey05@gmail.com>
2024-09-02 23:01:31 +0530

    Blog Details section added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Courses from './components/Courses/Courses';
 import Footer from './components/Footer/Footer';
 import Home from './components/HomeSection/Home';
 import Literature from './components/Literature/Literature';
+import BlogDetails from './components/MainNav/Other/BlogDetails';
 import Calendar02 from './components/MainNav/Other/Calendar02';
 import Calender01 from './components/MainNav/Other/Calender01';
 import Search from './components/MainNav/Other/Search';
@@ -27,6 +28,7 @@ function App() {
           <Route exact path='/search' element={<Search />} />
           <Route exact path='/course-cal-create-01' element={<Calender01 />} />
           <Route exact path='/Course-Calendar-Create2' element={<Calendar02 />} />
+          <Route exact path='/blog-details' element={<BlogDetails />} />
         </Routes>
         <Footer />
       </Router>
